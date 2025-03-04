5f156a6adb8cd83d79e4744a32630372cd9b0ad9
richcire <eric9907@naver.com>
2025-03-04 09:24:14 +0900

    give min width to bottom ads

--- components/google-adsense/bottom-display-ad-wrapper.tsx
+++ components/google-adsense/bottom-display-ad-wrapper.tsx
@@ -4,7 +4,7 @@ import { BottomDisplayAd } from "./bottom-display-ad";
 
 export function BottomDisplayAdWrapper() {
   return (
-    <div className="my-12 max-w-[768px] mx-auto px-4 h-40 w-full">
+    <div className="my-12 max-w-[768px] min-w-[320px] mx-auto px-4 h-40 w-full">
       <BottomDisplayAd />
     </div>
   );
