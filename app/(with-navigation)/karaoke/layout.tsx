c8273462e4230c386de5a2fac7e9d9a856e5d6ec
richcire <eric9907@naver.com>
2025-03-17 21:34:40 +0900

    add opengraph to metadata

---
+++ app/(with-navigation)/karaoke/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "🔍 JPOP 노래방 검색기",
+  description: "부르고 싶은 JPOP 노래의 노래방 번호를 찾아보세요!",
+};
+
+export default function KaraokeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
