fb68dbc221b24bf9fd624caf07f5d43dc2ecb3b1
richcire <eric9907@naver.com>
2025-03-31 20:46:49 +0900

    add notification feature for admin

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,17 +5,54 @@ import Link from "next/link";
 import { createClient } from "@/utils/supabase/client";
 import { User } from "@supabase/supabase-js";
 import Image from "next/image";
-import { IUserInfo } from "@/types/supabase-table";
+import { INotifications, IUserInfo } from "@/types/supabase-table";
 import { useRouter } from "next/navigation";
-import { User as UserIcon, ChevronRight } from "lucide-react";
+import { User as UserIcon, ChevronRight, BellIcon } from "lucide-react";
 const Navigation = () => {
   const [user, setUser] = useState<User | undefined>();
   const [name, setName] = useState<string | undefined>();
   const [showDropdown, setShowDropdown] = useState(false);
   const [showKaraokeDropdown, setShowKaraokeDropdown] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [notifications, setNotifications] = useState<INotifications[]>([]);
   const supabase = createClient();
   const router = useRouter();
+
+  const markAsReadAll = async () => {
+    if (notifications.length === 0) {
+      return;
+    }
+    const { data, error } = await supabase.from("notifications").upsert(
+      notifications.map((notification) => ({
+        ...notification,
+        is_read: true,
+      })),
+      {
+        onConflict: "id",
+      }
+    );
+    setNotifications([]);
+  };
+
+  const getNotifications = async (user_id: string, role: string) => {
+    const { data, error } =
+      role === "admin"
+        ? await supabase
+            .from("notifications")
+            .select("*")
+            .eq("is_read", false)
+            .order("created_at", { ascending: false })
+            .returns<INotifications[]>()
+        : await supabase
+            .from("notifications")
+            .select("*")
+            .eq("recipient_user_id", user_id)
+            .eq("is_read", false)
+            .order("created_at", { ascending: false })
+            .returns<INotifications[]>();
+    return { data, error };
+  };
 
   useEffect(() => {
     const getSession = async () => {
@@ -25,13 +62,23 @@ const Navigation = () => {
       }
       setUser(data.session?.user);
 
-      const { data: userData } = await supabase
-        .from("user_info")
-        .select("name")
-        .eq("user_id", data.session?.user.id)
-        .single<IUserInfo>();
+      if (data.session?.user) {
+        const { data: userData } = await supabase
+          .from("user_info")
+          .select("name, role")
+          .eq("user_id", data.session?.user.id)
+          .single<IUserInfo>();
 
-      setName(userData?.name);
+        setName(userData?.name);
+
+        const { data: notifications } = await getNotifications(
+          data.session?.user.id,
+          userData?.role || ""
+        );
+        if (notifications) {
+          setNotifications(notifications);
+        }
+      }
     };
     getSession();
   }, []);
@@ -111,7 +158,65 @@ const Navigation = () => {
             </div>
 
             {/* Auth Section */}
+
             <div className="flex items-center justify-end gap-2 sm:gap-4 col-span-1 sm:col-span-2 lg:col-span-1">
+              {/* Notifications Section */}
+              <div>
+                <button
+                  className="hover:text-primary transition-colors flex items-center justify-center relative"
+                  onClick={() => setShowNotifications((prev) => !prev)}
+                >
+                  <BellIcon size={20} />
+                  {notifications.length > 0 && (
+                    <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+                      {notifications.length > 9 ? "9+" : notifications.length}
+                    </div>
+                  )}
+                </button>
+
+                {showNotifications && (
+                  <div className="absolute right-1 top-full pt-2">
+                    <div className="w-80 bg-[#F5F5F5] backdrop-blur-sm shadow-sm border rounded-md p-4">
+                      <div className="flex items-center justify-between mb-4">
+                        <h3 className="font-medium">알림</h3>
+                        <button
+                          className="text-sm text-muted-foreground hover:text-primary transition-colors"
+                          onClick={markAsReadAll}
+                        >
+                          모두 읽음 표시
+                        </button>
+                      </div>
+
+                      <div className="space-y-4">
+                        {notifications.length > 0 ? (
+                          notifications.map((notification) => (
+                            <Link
+                              href={notification.relevant_url || "#"}
+                              key={notification.id}
+                              className="flex items-start gap-3 p-2 rounded-md"
+                            >
+                              <div className="flex-1">
+                                <p className="text-sm">
+                                  {notification.message}
+                                </p>
+                                <p className="text-xs text-muted-foreground">
+                                  {new Date(
+                                    notification.created_at
+                                  ).toLocaleDateString()}
+                                </p>
+                              </div>
+                            </Link>
+                          ))
+                        ) : (
+                          <div className="text-sm text-center text-muted-foreground">
+                            새로운 알림이 없습니다
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
               {user ? (
                 <div
                   className="relative"
