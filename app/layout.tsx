0f7f33b1c07fefbfd6d5710dcc6954444f5bcb09
richcire <eric9907@naver.com>
2025-04-13 22:00:19 +0900

    apply font in local file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import { Gothic_A1 } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { WebVitals } from "@/components/web-vitals";
 import { AdSenseCodeSnippet } from "@/components/google-adsense/adsense-code-snippet";
 import { Toaster } from "@/components/ui/sonner";
+import localFont from "next/font/local";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -31,9 +31,54 @@ export const metadata = {
   },
 };
 
-const gothicFont = Gothic_A1({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+const gothica1Font = localFont({
+  src: [
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gothic_A1/GothicA1-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   display: "swap",
 });
 
@@ -43,7 +88,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={gothicFont.className} suppressHydrationWarning>
+    <html lang="ko" className={gothica1Font.className} suppressHydrationWarning>
       <head>
         {/* 구글 애드센스 코드 스니펫 */}
         <AdSenseCodeSnippet />
