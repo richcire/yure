ec24fc5c45d2e09496ff2d5a1f56c5f50d8339be
richcire <eric9907@naver.com>
2025-05-06 15:33:52 +0900

    reduce number of  image formats to one

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     // loader: "custom",
     // loaderFile: "./utils/supabase/supabase-image-loader.ts",
     remotePatterns: [
