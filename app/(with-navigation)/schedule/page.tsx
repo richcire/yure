41037bebee1c2fd51e1dc04cd704e215665f6419
성태경 <taegyeong0320@naver.com>
2025-05-07 12:49:20 +0900

    show list when search

--- app/(with-navigation)/schedule/page.tsx
+++ app/(with-navigation)/schedule/page.tsx
@@ -1,15 +1,25 @@
 import Calendar from "@/components/schedule/calendar";
-
+import Header from "@/components/schedule/schedule-header";
+import ScheduleList from "@/components/schedule/schedule-list";
 // import interactionPlugin from "@fullcalendar/interaction";
 
 // const handleDateClick = () => {
 //   alert("날짜 선택됨");
 // };
 
-export default function SchedulePage() {
+interface Props {
+  searchParams: Promise<{
+    search?: string;
+    page?: string;
+  }>;
+}
+
+export default async function SchedulePage({ searchParams }: Props) {
+  const { search, page = "1" } = await searchParams;
   return (
     <div className="w-full max-w-5xl mx-auto min-h-screen bg-background">
-      <Calendar />
+      <Header />
+      {search ? <ScheduleList /> : <Calendar />}
     </div>
   );
 }
