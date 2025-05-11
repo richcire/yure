3eb863c5124cfa5b202b90e09873b9769edf951e
성태경 <taegyeong0320@naver.com>
2025-05-11 16:19:48 +0900

    add calendar wrapper and schedule card wrapper

--- components/schedule/schedule-list.tsx
+++ components/schedule/schedule-list.tsx
@@ -1,73 +1,27 @@
-import { PaginationControl } from "@/components/ui/pagination-control";
-import { IEvents } from "@/types/supabase-table";
-import { createClient } from "@/utils/supabase/server";
 import { BottomDisplayAdWrapper } from "../google-adsense/bottom-display-ad-wrapper";
-import ScheduleCard from "@/components/schedule/schedule-card";
-import Calendar from "@/components/schedule/calendar";
-
-const ITEMS_PER_PAGE = 9;
+import ScheduleCardWrapper from "@/components/schedule/schedule-card-wrapper";
+import CalendarWrapper from "@/components/schedule/calendar-wrapper";
 
 interface Props {
   searchParams: Promise<{
-    search?: string; // 검색 키워드
+    search?: string;
     page?: string;
     sort?: string;
   }>;
 }
 
 export default async function ScheduleList({ searchParams }: Props) {
-  const { search, page = "1", sort } = await searchParams;
-  const currentPage = parseInt(page);
-  const supabase = await createClient();
-
-  // Build the base query
-  let query = supabase.from("events").select(
-    `
-      id,
-      title,
-      description,
-      event_types!inner (id, name),
-      start_date,
-      end_date,
-      created_at
-    `,
-    { count: "exact" }
-  );
-
-  if (search) {
-    // Convert search term to lowercase for case-insensitive search
-    const searchTerm = search.toLowerCase();
-    query = query.or(`title.ilike.%${searchTerm}%`);
-  }
-
-  // 이벤트 타입으로 필터링
-  if (sort) {
-    query = query.eq("event_types.name", sort);
-  }
-
-  // Fetch paginated data with sorting
-  const { data: events, count } = await query
-    .order("created_at", { ascending: false })
-    .range((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE - 1)
-    .returns<IEvents[]>();
-
-  const totalPages = Math.ceil((count || 0) / ITEMS_PER_PAGE);
+  const { search } = await searchParams;
 
   return (
     <>
-      {events ? (
-        search ? (
-          <ScheduleCard events={events} />
-        ) : (
-          <Calendar events={events} />
-        )
+      {/* 검색어가 없으면 캘린더, 있으면 리스트 */}
+      {!search ? (
+        <CalendarWrapper searchParams={searchParams} />
       ) : (
-        ""
+        <ScheduleCardWrapper searchParams={searchParams} />
       )}
       <BottomDisplayAdWrapper />
-      {totalPages > 1 && (
-        <PaginationControl currentPage={currentPage} totalPages={totalPages} />
-      )}
     </>
   );
 }
