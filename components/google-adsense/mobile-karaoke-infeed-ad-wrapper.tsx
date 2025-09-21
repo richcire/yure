4eeab53b38fb4b57badb50e11bca938f55a09a36
richcire <eric9907@naver.com>
2025-09-21 14:57:24 +0900

    fix infeed ad size

--- components/google-adsense/mobile-karaoke-infeed-ad-wrapper.tsx
+++ components/google-adsense/mobile-karaoke-infeed-ad-wrapper.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useEffect } from "react";
 import { BottomDisplayAd } from "./bottom-display-ad";
 
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
+
 export function MobileKaraokeInfeedAdWrapper() {
+  if (process.env.NODE_ENV !== "production") return null;
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
   return (
-    <div className="my-12 max-w-[768px] min-w-[320px] mx-auto px-4 h-20 w-full">
-      <BottomDisplayAd />
+    <div className="my-12 max-w-[768px] min-w-[320px] mx-auto px-4 w-full">
+      <ins
+        className="adsbygoogle h-20"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-4738868818137222"
+        data-ad-slot="6505812562"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
     </div>
   );
 }
