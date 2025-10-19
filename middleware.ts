09b020b7c98d6aee46c672c663d53f29ef09c711
richcire <eric9907@naver.com>
2025-10-19 13:20:46 +0900

    disable redirecting old wordpress url

--- middleware.ts
+++ middleware.ts
@@ -1,64 +1,42 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
-import { createClient } from "./utils/supabase/server";
+// import { createClient } from "./utils/supabase/server";
 
 export async function middleware(request: NextRequest) {
-  // const baseUrl = request.nextUrl.origin;
-  // const originalPath = request.nextUrl.pathname;
-
-  // console.log(originalPath);
-  // // 페이지 존재 여부 확인
-  // const res = await fetch(`${baseUrl}${originalPath}`, { method: "HEAD" });
-
-  // if (res.status === 404) {
-  //   const supabase = await createClient();
-  //   const { data: translation } = await supabase
-  //     .from("translations")
-  //     .select("id")
-  //     .eq("permalink", originalPath.slice(1))
-  //     .single();
-
-  //   if (translation) {
-  //     return NextResponse.redirect(
-  //       new URL(`/translation/${translation.id}`, request.url)
-  //     );
-  //   }
-  // }
-
-  const { pathname } = request.nextUrl;
+  // const { pathname } = request.nextUrl;
 
   //redirect old wordpress url to new url
   // Define routes to ignore (not to be redirected)
-  const excludePaths = [
-    "/translation",
-    "/article",
-    "/karaoke",
-    "/news",
-    "/schedule",
-    "/admin",
-    "/api",
-    "/terms",
-    "/_next",
-    "/favicon.ico",
-    "/sitemap-index.xml",
-    "/sitemap.xml",
-    "/sign-in",
-    "/auth",
-    "/protected",
-    "/ads.txt",
-    "/schedule",
-    "/assets",
-  ];
+  // const excludePaths = [
+  //   "/translation",
+  //   "/article",
+  //   "/karaoke",
+  //   "/news",
+  //   "/schedule",
+  //   "/admin",
+  //   "/api",
+  //   "/terms",
+  //   "/_next",
+  //   "/favicon.ico",
+  //   "/sitemap-index.xml",
+  //   "/sitemap.xml",
+  //   "/sign-in",
+  //   "/auth",
+  //   "/protected",
+  //   "/ads.txt",
+  //   "/schedule",
+  //   "/assets",
+  // ];
 
-  const isExcluded = excludePaths.some((path) => pathname.startsWith(path));
+  // const isExcluded = excludePaths.some((path) => pathname.startsWith(path));
 
   // If it's a root-level path like /some-slug (e.g. no other slashes)
-  const isSlugOnly = pathname.split("/").filter(Boolean).length === 1;
+  // const isSlugOnly = pathname.split("/").filter(Boolean).length === 1;
 
-  if (!isExcluded && isSlugOnly) {
-    const newUrl = new URL(`/translation/${pathname}`, request.url);
-    return NextResponse.redirect(newUrl, 301);
-  }
+  // if (!isExcluded && isSlugOnly) {
+  //   const newUrl = new URL(`/translation/${pathname}`, request.url);
+  //   return NextResponse.redirect(newUrl, 301);
+  // }
 
   return await updateSession(request);
 }
