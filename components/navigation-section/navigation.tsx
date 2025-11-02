5f7e607d47db7ca7e68b3239f13d35ea7d35cd5b
richcire <eric9907@naver.com>
2025-11-02 20:24:39 +0900

    fix mobile navigation design

--- components/navigation-section/navigation.tsx
+++ components/navigation-section/navigation.tsx
@@ -78,7 +78,7 @@ const Navigation = () => {
         <div className="container mx-auto h-full px-4">
           <div className="flex items-center justify-between h-full">
             {/* Logo */}
-            <Link href="/" className="flex items-center gap-2 w-[150px]">
+            <Link href="/" className="flex items-center gap-2 md:w-[150px]">
               <Image
                 src={roundLogo}
                 alt="Logo"
@@ -147,7 +147,7 @@ const Navigation = () => {
             </div>
 
             {/* Auth Section */}
-            <div className="flex items-center gap-3 md:gap-4 w-[150px]">
+            <div className="flex items-center gap-3 md:gap-4 md:w-[150px]">
               {/* Notifications Section */}
               {user && <Notification />}
               <div className="hidden sm:block">
